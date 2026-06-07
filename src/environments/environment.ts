c50976978352138aa7f426c5a8c793bdf7d3c311
smsproject2025 <bulkbulksmsproject2025@gmail.com>
2026-06-07 22:01:59 +0530

    env file updated

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,5 @@
-// export const environment = {
-//   production: false,
-//   apiUrl: 'http://localhost:8080/api',
-//   razorpayKeyId: 'rzp_test_xxxxxxxxxxxxxxxx'
-// };
-
 export const environment = {
-  production: true,
+  production: false,
   apiUrl: 'https://sms-portal-backend-ldm5.onrender.com/api',
   razorpayKeyId: 'rzp_test_SvZsX3eaDxFbHw'
 };
